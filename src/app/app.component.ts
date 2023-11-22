7700d5417b60935e5c1d74a98cf66df0d92e9775
AnaRebo15 <anapaorebo15@gmail.com>
2023-11-22 08:03:46 -0600

    Modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'CBDF_APRS_09';
+  title = 'Ejercicio 09 - Interpolación';
+  nombre = "Ana Paola Rebolloso Saucedo";
+  edad = 25;
+  semestre = "5to semestre";
+  calificaciones = [8,9,10,8,9,9,9,10,9];
+
+  calcularPromedios(){
+    let suma = 0;
+    let longitudArreglo = this.calificaciones.length;
+    for(let i=0; i<longitudArreglo; i++)
+      suma+=this.calificaciones[i];
+    return suma/longitudArreglo;
+  }
+
+  estadoAlumno(){
+    if(this.calcularPromedios()>=6)
+      return "APROBADO";
+    else
+      return "REPROBADO";
+  }
 }
